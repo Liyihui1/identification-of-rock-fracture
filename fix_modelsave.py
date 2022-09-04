ab88c3594f696790f42fcda59891a725cc0fa0bc
Liyihui1 <92657007+Liyihui1@users.noreply.github.com>
2022-09-04 20:35:26 +0800

    Delete fix_modelsave.py

--- fix_modelsave.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-from networcks.linknet import DinkNet34
-modelname = 'DinkNet34'
-modelloadpath = 'weights/liexi/DinkNet34_weights_17_0.8315882855349298.pth'
-net = eval(modelname)(num_classes=1)
-state_dict = torch.load(modelloadpath)
-net.load_state_dict(state_dict)
-torch.save(net.state_dict(), modelloadpath, _use_new_zipfile_serialization = False)
